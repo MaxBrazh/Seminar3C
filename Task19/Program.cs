a7efebb4ede1fc95091723756d6ee21d696bb653
Maksim <brazhmax@mail.ru>
2022-08-12 11:17:36 +0300

    add start Task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+using static System.Console;
