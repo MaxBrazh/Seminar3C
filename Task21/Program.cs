dc6c396fadb9db1625f8add99d12038579ed53c3
Maksim <brazhmax@mail.ru>
2022-08-17 11:55:38 +0300

    add Task 21 full

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,21 @@
 ﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
 using static System.Console;
+Clear();
+
+WriteLine("Введите координату х для точки A: ");
+int x1 = int.Parse(ReadLine());
+WriteLine("Введите координату y для точки A: ");
+int y1 = int.Parse(ReadLine());
+WriteLine("Введите координату z для точки A: ");
+int z1 = int.Parse(ReadLine());
+WriteLine("Введите координату x для  точки B: ");
+int x2 = int.Parse(ReadLine());
+WriteLine("Введите координату y для  точки B: ");
+int y2 = int.Parse(ReadLine());
+WriteLine("Введите координату z для  точки B: ");
+int z2 = int.Parse(ReadLine());
+
+double dist = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)+ Math.Pow((z2 - z1), 2));
+
+WriteLine($"Расстояние между двумя точками в 3D пространстве: {dist:f2}");
--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 using static System.Console;
+Clear();
+
+WriteLine("Введите любое число: ");
+int number = int.Parse(ReadLine());
+WriteLine("Таблица кубов чисел от 1 до заданного числа:");
+
+int n = 1;
+while (n <= number)
+{
+    WriteLine($"{n} в кубе = {n * n * n}");
+    n++;
+}
